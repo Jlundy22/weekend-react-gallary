fab5e21600dcde9d6160ca63b74f5b1fb5064fc6
Jack Lund <jacklund920@gmail.com>
2022-04-30 10:39:18 -0500

    inital commit, added pics and descriptions

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,11 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/Cali-wedding.jpg', description: 'Out friends wedding in Napa California.', likes: 0 },
+    { id: 3, path: 'images/disc.png', description: 'My favorite hobby: Disc golf.', likes: 0 },
+    { id: 4, path: 'images/goodboy-rusko.jpg', description: 'My dog rusko.', likes: 0 },
+    { id: 5, path: 'images/lake-rusko.jpg', description: 'Rusko relaxing at the lake.', likes: 0 },
+    { id: 6, path: 'images/more-rusko.jpg', description: 'Rusko in camouflage.', likes: 0 },
+    { id: 7, path: 'images/thanksgiving.jpg', description: 'Thanksgiving feast 2020.', likes: 0 }
 ];
 
 module.exports = galleryItems;
