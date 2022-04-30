84945be8b9aec02eb4828a71a604290322966214
Jack Lund <jacklund920@gmail.com>
2022-04-30 14:26:05 -0500

    PICTURES! Now with 100 percent more toggles

--- src/components/App/GalleryItem.jsx
+++ src/components/App/GalleryItem.jsx
@@ -1,17 +1,30 @@
+import { useState } from 'react';
 
 function GalleryItem({ galleryItem }) {
+    const [displayPicture, setDisplayPicture] = useState(true);
+
+    const pictureClick = () => {
+        setDisplayPicture(!displayPicture);
+    }
+
     return (
         <div>
-            <img src={galleryItem.path} width="100" height="100" />
-            {galleryItem.description}
-            <button>LIKE</button>
-            Likes: {galleryItem.likes}
+            {displayPicture &&
+                <div>
+                    <img onClick={pictureClick} src={galleryItem.path} alt="" width="100" height="100" />
+                    <button>LIKE</button>
+                    Likes: {galleryItem.likes}
+                </div>
+            }
+            {!displayPicture &&
+                <div>
+                    <div onClick={pictureClick}>{galleryItem.description}</div>
+                    <button>LIKE</button>
+                    Likes: {galleryItem.likes}
+                </div>
+            }
         </div>
     )
-
-
-
-
 }
 
 export default GalleryItem;
