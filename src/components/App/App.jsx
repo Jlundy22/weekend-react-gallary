192be6c48032d834613b3ef118f3aa5a9907535b
Jack Lund <jacklund920@gmail.com>
2022-04-30 11:53:13 -0500

    GET route semi-function, pic,description , and likes append to the dom

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,49 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 import Header from './Header.jsx'
 
 function App() {
-    return (
-      <div className="App">
-        < Header />
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
-      </div>
-    );
+  useEffect(() => {
+    fetchGallery();
+  },[])
+
+
+  const [galleryList, setGalleryList] = useState([]);
+
+  const fetchGallery = () => {
+    axios({
+      method: 'GET',
+      url: '/gallery'
+    }).then((response) => {
+      console.log(response.data);
+      setGalleryList(response.data);
+      console.log(galleryList);
+    }).catch((error) => {
+      console.log('GET /gallery error',error)
+    })
+  }
+
+
+
+  return (
+    <div className="App">
+      < Header />
+      <ul>
+        {galleryList.map((galleryItem) => {
+          return (
+            <li key={galleryItem.id}>
+              <img src={galleryItem.path} width="100" height="100"/>
+              {galleryItem.description}
+              <button>LIKE</button>
+              Likes: {galleryItem.likes}
+            </li>
+          )
+        })}
+      </ul>
+
+    </div>
+  );
 }
 
 export default App;
